a0d8a0f19f6a34e05799268fd5c40bdfc96c91d9
arriagareyes <omararriagareyes@outlook.com>
2021-05-31 00:40:40 -0400

    Added a basic game loop.

--- main.cpp
+++ main.cpp
@@ -8,13 +8,42 @@ struct Game : Basics {
         cout << "Initalizing game" << endl;
     }
 
+private:
+    int map[2]{ 20, 10 };
+    int player[2] { 0, 0 };
+
 protected:
     void update() override {
-        cout << "I am updating!" << endl;
+
     }
 
     void render() override {
-        cout << "I am rendering!" << endl;
+        for(size_t x = 0; x < map[0] + 3; x++) {
+            cout << "#";
+        }
+
+        cout << endl;
+
+        for(size_t y = 0; y < map[1] + 1; y++) {
+            for(size_t x = 0; x < map[0] + 2; x++) {
+                if(x == 0) {
+                    cout << "#";
+                } else {
+                    cout << " ";
+                }
+            }
+
+            cout << "#" << endl;
+        }
+
+        for(size_t x = 0; x < map[0] + 3; x++) {
+            cout << "#";
+        }
+
+        cout << endl;
+
+        //Have to find a better way to clear console
+        //std::system("clear");
     }
 };
 
--- src/basics.cpp
+++ src/basics.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <chrono>
 #include "../include/basics.hpp"
 
 void Basics::run() {
+    using milliseconds = std::chrono::duration<double, std::milli>;
+
+    const auto MS_PER_UPDATE = 0.0017;
+
+    auto previous = std::chrono::steady_clock::now();
+    double lag = 0.0;
     while(true) {
+        auto current = std::chrono::steady_clock::now();
+        auto delta = current - previous;
+        previous = current;
+        lag += milliseconds(delta).count();
+
+        while(lag >= MS_PER_UPDATE) {
+            update();
+            lag -= MS_PER_UPDATE;
+        }
+
         render();
-        update();
     }
 }
